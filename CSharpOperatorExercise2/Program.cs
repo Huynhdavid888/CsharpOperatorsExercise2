4268d8e47d51f24279e62e7e13b128e66e18157b
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-13 23:52:22 -0700

    initial commit

---
+++ CSharpOperatorExercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharpOperatorExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What is the radius of your circle?");
+           
+            var radius = double.Parse(Console.ReadLine());
+
+            var r = 30;
+            var pi = Math.PI;
+            var areaOfCircle = Math.PI * (radius * radius);
+            Console.WriteLine($"The area of a circle with radius {r} is {areaOfCircle}");
+        }
+    }
+}
